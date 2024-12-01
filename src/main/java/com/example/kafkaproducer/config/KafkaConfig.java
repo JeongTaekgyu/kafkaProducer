b67656b54719415406b8e0308714a8415c248b7d
JeongTaekgyu <tk.jeong@bitnine.net>
2024-12-01 21:45:09 +0900

    리스너를 통해서 컨슈머가 데이터를 정상적으로 받아오는지 확인

--- src/main/java/com/example/kafkaproducer/config/KafkaConfig.java
+++ src/main/java/com/example/kafkaproducer/config/KafkaConfig.java
@@ -2,10 +2,12 @@ package com.example.kafkaproducer.config;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.*;
 
 import java.util.HashMap;
@@ -29,11 +31,30 @@ public class KafkaConfig {
 //        myConfig.put("bootstrap.servers", "localhost:9092");
 
         // 임의 로 아무 ip나 넣었는데 이거는 public ip를 넣어야한다. 왜냐하면 aws 안에서 돌리는게 아니라 public ip를 넣는다.
-        myConfig.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "172.31.0.123:9092, 172.31.4.62:9092, 172.31.13.550");
+        myConfig.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "172.31.0.147:9092, 172.31.4.6:9092, 172.31.13.150:9092");
         myConfig.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         myConfig.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
 
         return new DefaultKafkaProducerFactory<>(myConfig);
     }
 
+    @Bean
+    public ConsumerFactory<String, Object> ConsumerFactory() {
+        Map<String, Object> myConfig = new HashMap<>();
+
+        myConfig.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "172.31.0.147:9092, 172.31.4.6:9092, 172.31.13.150:9092");
+        myConfig.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        myConfig.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+
+        return new DefaultKafkaConsumerFactory<>(myConfig);
+    }
+
+    // Kafka 브로커로부터 메시지를 수신할 컨테이너를 관리한다.
+    // ConsumerFactory를 지정해 주는 것이다. 그러면 그 리스너가 그 ConsumerFactory를 사용해서 concurrent하게 데이터를 가져온다.
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, Object> kafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, Object> myfactory = new ConcurrentKafkaListenerContainerFactory<>();
+        myfactory.setConsumerFactory(ConsumerFactory());
+        return myfactory;
+    }
 }
---
+++ src/main/java/com/example/kafkaproducer/service/ConsumerService.java
@@ -0,0 +1,14 @@
+package com.example.kafkaproducer.service;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ConsumerService {
+
+    // 참고로 topic을 받을때 컨슈머 그룹안에 컨슈머가 있다.
+    @KafkaListener(topics = "defaultTopic", groupId = "spring")
+    public void consumer(String message) {
+        System.out.println(String.format("Subscribed message: %s" + message));
+    }
+}
