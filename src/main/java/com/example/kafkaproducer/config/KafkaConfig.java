6d9e017d0abe183433ce839cb81d1abcc3ef62f3
JeongTaekgyu <tk.jeong@bitnine.net>
2024-12-03 00:39:46 +0900

    KafkaStream을 활용한 buildPipeline 확인

--- src/main/java/com/example/kafkaproducer/config/KafkaConfig.java
+++ src/main/java/com/example/kafkaproducer/config/KafkaConfig.java
@@ -2,20 +2,42 @@ package com.example.kafkaproducer.config;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.config.TopicConfig;
+import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.apache.kafka.streams.StreamsConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.annotation.EnableKafkaStreams;
+import org.springframework.kafka.annotation.KafkaStreamsDefaultConfiguration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.config.KafkaStreamsConfiguration;
 import org.springframework.kafka.core.*;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @Configuration
+@EnableKafkaStreams
 @EnableKafka
 public class KafkaConfig {
+
+    @Bean(name = KafkaStreamsDefaultConfiguration.DEFAULT_STREAMS_CONFIG_BEAN_NAME)
+    public KafkaStreamsConfiguration myKStreamConfig() {
+        Map<String, Object> myKStreamConfig = new HashMap<>();
+        myKStreamConfig.put(StreamsConfig.APPLICATION_ID_CONFIG, "stream-test");
+        myKStreamConfig.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "172.31.0.147:9092, 172.31.4.6:9092, 172.31.13.150:9092");
+                                        // 스트림은 받고 바로 보내고 하기 때문에 SERIALIZER, DESERIALIZER를 하지 않고 한 번에 선언을 해준다.
+        myKStreamConfig.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+        myKStreamConfig.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+        myKStreamConfig.put(StreamsConfig.NUM_STREAM_THREADS_CONFIG, 3);
+//        myKStreamConfig.put(StreamsConfig.producerPrefix(ProducerConfig.ACKS_CONFIG), "all");
+//        myKStreamConfig.put(StreamsConfig.topicPrefix(TopicConfig.MIN_IN_SYNC_REPLICAS_CONFIG), 2);
+//        myKStreamConfig.put(StreamsConfig.NUM_STANDBY_REPLICAS_CONFIG, 1);
+        return new KafkaStreamsConfiguration(myKStreamConfig);
+    }
 
     // 이 패턴이 템플릿을 만들고 커넥션 정보가 들어간 configuration을 만든다.
     // 그리고 그 configuration 메서드(여기서는 ProducerFactory())를 호출해서
